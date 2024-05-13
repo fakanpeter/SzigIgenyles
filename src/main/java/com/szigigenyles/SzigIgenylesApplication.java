09e92b647fa9a0b6b96074dadbd47e69cbc13d5b
fakanpeter <fakanpeti@gmail.com>
2024-05-13 19:11:39 +0200

    Initial commit

---
+++ src/main/java/com/szigigenyles/SzigIgenylesApplication.java
@@ -0,0 +1,13 @@
+package com.szigigenyles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzigIgenylesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzigIgenylesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szigigenyles/SzigIgenylesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szigigenyles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzigIgenylesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
